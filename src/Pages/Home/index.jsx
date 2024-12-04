20998b89f52e8877463d9bb6cb957e02a5b1df1c
Rafael Thomaz <rafael.hthomaz123@gmail.com>
2024-12-04 16:45:14 -0300

    cards

--- src/Pages/Home/index.jsx
+++ src/Pages/Home/index.jsx
@@ -1,34 +1,56 @@
 import api from '../../Services/api'
 import React, { useEffect, useState } from 'react'
+import '../../App.css'
+function Home() {
 
-function Home(){
-    
-  const[produtos,setProdutos] = useState([])
+  const [produtos, setProdutos] = useState([])
+  const [carregando, setCarregando] = useState(true)
 
-  useEffect(()=>{
+  useEffect(() => {
     async function carregandoAPI() {
       const response = await api.get('/products')
       setProdutos(response.data)
+      setCarregando(false)
     }
     carregandoAPI()
-    
-  },[])
 
+  }, [])
 
+  if (carregando) {
+    return (
+      <div>
+        <h1>Carregando...</h1>
+      </div>
+    )
+
+  }
   return (
-    <>
-      {produtos.map((item)=>{
-        return(
-          <div key={item.id}>
-            <h1>{item.title}</h1>
-            <strong>{item.price}</strong>
-            <img src={item.image} alt={item.title} />
-            <p>{item.description}</p>
-            <strong>{item.category}</strong>
-          </div>
-        )
-      })}
-    </>
+    <div className="container">
+      <div className="row">
+        {produtos.map((item) => {
+          return (
+            <div className="col-md-3 mb-4" key={item.id}>
+              <div className="card h-100">
+                <img src={item.image} className="card-img-top" alt={item.title} />
+                <div className="card-body"> 
+
+                  {/* Esse codigo esta chamando o titulo da API definindo um tamanho maximo de 40 caracter */}
+                  <h5 className="card-title">{item.title.length > 40 ? `${item.title.substring(0,40)}...` : item.title}</h5>
+                 
+                  <p className="card-text">
+                    {item.description.length > 100 ? `${item.description.substring(0, 100)}...` : item.description}
+                  </p>
+
+                  <strong className="card-text">{item.price}</strong>
+                  <p className="text-muted">{item.category}</p>
+                </div>
+              </div>
+            </div>
+          )
+        })}
+      </div>
+    </div>
+
   )
 }
 
